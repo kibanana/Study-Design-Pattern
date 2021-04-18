afe5a82eeacb07c768604b945388f9dc42b0503c
Kim Yewon <kyw017763@gmail.com>
2021-04-19 07:43:50 +0900

    Add factory method pattern elevator example

---
+++ src/factory_method/way1/Direction.java
@@ -0,0 +1,3 @@
+package factory_method.way1;
+
+public enum Direction { UP, DOWN }
---
+++ src/factory_method/way1/ThroughputScheduler.java
@@ -0,0 +1,7 @@
+package factory_method.way1;
+
+public class ThroughputScheduler { // 엘리베이터 작업 처리량을 최대화시키는 전략 클래스
+    public int selectElevator(ElevatorManager manager, int destination, Direction direction) {
+        return 0; // 임의로 선택함
+    }
+}
